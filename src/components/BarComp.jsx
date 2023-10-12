d8c2b6e53d02d691ce30a039d124cea33c92d3e2
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-10-12 17:52:54 +0700

    edit:barcomp

--- src/components/BarComp.jsx
+++ src/components/BarComp.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const BarComp = ({ customClasses, iconComp, tes, home }) => {
     return (
         <div className={customClasses}>
-            <aside className=" " aria-label="Sidebar">
-                <div className="bg-white border-t border-gray-300 w-full min-h-[4rem] ">
+            <aside className="flex justify-center items-center" aria-label="Sidebar">
+                <div className="bg-[#1053f3] rounded-full w-[75%] min-h-[3rem] ">
                     <div className="w-full flex items-center justify-between">
-                        <div className="w-1/2 flex justify-start mt-2 px-2">{iconComp}</div>
-                        <div className="w-1/2 flex justify-end mt-2 px-2">{tes}</div>
+                        <div className="w-1/2 flex justify-start  ">{iconComp}</div>
+                        <div className="w-1/2 flex justify-end ">{tes}</div>
                     </div>
                     {home}
                 </div>
