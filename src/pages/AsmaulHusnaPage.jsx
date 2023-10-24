36f3904256e5650b2e403c005b4cf35b4a276c64
caturanggaadiyaksa <adiyaksacaturangga@gmail.com>
2023-10-24 09:19:38 +0700

    edit(asmaulhusnapage)

--- src/pages/AsmaulHusnaPage.jsx
+++ src/pages/AsmaulHusnaPage.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import AsmaulHusna from '../components/navbar/AsmaulHusna';
+import AsmaulHusnaCard from '../components/cards/AsmaulHusnaCard';
 
 const AsmaulHusnaPage = () => {
-  return <div>Halaman Asmaul Husna</div>;
+  return(
+    <div>
+      <AsmaulHusna/>
+
+      <div className='pt-[6rem] w-full'>
+        <div className='w-[80%] mx-auto'>
+
+          <div className="w-full">
+            <div className='flex justify-center items-center'>
+              <AsmaulHusnaCard/>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AsmaulHusnaPage;
