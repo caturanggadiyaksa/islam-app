6e4861839b77e1f9bb6dace0b2091fa92001129d
caturanggaadiyaksa <adiyaksacaturangga@gmail.com>
2023-10-24 09:16:15 +0700

    remove(tag a to div)

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -15,40 +15,34 @@ const Footer = () => {
                 <div className="bg-[#1053f3] rounded-full w-[75%] min-h-[3rem] ">
                     <div className="w-full flex items-center justify-between pt-1 px-4">
                         <Link to="/">
-                            <a
-                                href="#"
-                                target="_blank"
-                                className="ml-4 flex items-center p-2 text-2xl font-normal text-gray-900 rounded-lg"
+                            <div className="ml-4 flex items-center p-2 text-2xl font-normal text-gray-900 rounded-lg"
                             >
                                 <FontAwesomeIcon icon={faHouseChimneyWindow} className="text-white" />
 
 
-                            </a>
+                            </div>
                         </Link>
 
 
                         <Link to="/Bookmark">
-                            <a
-                                href="#"
-                                target="_blank"
-                                className=" flex items-center p-2 text-2xl font-normal text-gray-900 rounded-lg "
+                            <div className=" flex items-center p-2 text-2xl font-normal text-gray-900 rounded-lg "
                             >
                                 <FontAwesomeIcon icon={faHeart} className="text-white" />
 
 
-                            </a>
+                            </div>
 
                         </Link>
 
                         <Link to="/account">
-                            <a
-                                href="#"
+                            <div
+                                
                                 className=" mr-4 flex items-center p-2 text-2xl font-normal text-gray-900 rounded-lg"
                             >
 
                                 <FontAwesomeIcon icon={faUserTie} className="text-white" />
 
-                            </a>
+                            </div>
 
                         </Link>
 
