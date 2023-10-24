92d5baae265c3099f349f1be23e322130e51288a
caturanggaadiyaksa <adiyaksacaturangga@gmail.com>
2023-10-24 09:17:03 +0700

    add(component) asmaul husna

---
+++ src/components/navbar/AsmaulHusna.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeftLong } from '@fortawesome/free-solid-svg-icons';
+
+
+
+const AsmaulHusna = () => {
+    return(
+        <>
+             <header
+            className="fixed top-0 z-50 w-full h-[5rem] bg-slate-50 shadow-gray-500 drop-shadow-md"
+        >
+            <div className=" w-full flex justify-center items-center">
+                <div className="w-full h-[5rem]">
+                    <div className="w-[90%] h-[5rem] mx-auto flex items-center justify-beetwen">
+                        <div className="w-1/2 flex items-center">
+                            <FontAwesomeIcon icon={faArrowLeftLong} />
+                            <div className="ml-4">
+                               Asmaul Husna
+                            </div>
+                        </div>
+
+
+                       
+
+
+
+                    </div>
+                </div>
+
+            </div>
+        </header>
+        </>
+    );
+};
+
+export default AsmaulHusna;
