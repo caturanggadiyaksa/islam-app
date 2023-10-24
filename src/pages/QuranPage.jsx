f52204a863d390aca4f28eac197b68a0d96ef290
caturanggaadiyaksa <adiyaksacaturangga@gmail.com>
2023-10-24 09:20:24 +0700

    edit(quranpage)

--- src/pages/QuranPage.jsx
+++ src/pages/QuranPage.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import NavbarQuran from '../components/navbar/qurannav/NavbarQuran';
+import QuranContainer from '../containers/QuranContainer';
+
 
 const QuranPage = () => {
-  return <div>Halaman Quran</div>;
+  return(
+    <div>
+      <NavbarQuran/>
+
+      <div className='pt-[6rem]'> 
+        <QuranContainer/>
+      </div>
+    </div>
+  );
 };
 
 export default QuranPage;
