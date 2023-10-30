f7fe0f756700f9c9a176a90c5dbb685c3f3eb110
caturanggaadiyaksa <adiyaksacaturangga@gmail.com>
2023-10-30 10:29:46 +0700

    add useeffect

--- src/pages/AsmaulHusnaPage.jsx
+++ src/pages/AsmaulHusnaPage.jsx
@@ -1,22 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import AsmaulHusna from '../components/navbar/AsmaulHusna';
 import AsmaulHusnaCard from '../components/cards/AsmaulHusnaCard';
+import Footer from '../components/footer/Footer';
 
 const AsmaulHusnaPage = () => {
-  return(
+
+
+  const [asmaulHusnaData, setAsmaulHusnaData] = useState([]);
+
+  useEffect(() => {
+
+    const data = localStorage.getItem('asmaulhusnaData');
+    if (data) {
+      setAsmaulHusnaData(JSON.parse(data));
+    }
+
+  }, []);
+
+
+  return (
     <div>
-      <AsmaulHusna/>
+      <AsmaulHusna />
 
       <div className='pt-[6rem] w-full'>
         <div className='w-[80%] mx-auto'>
 
           <div className="w-full">
-            <div className='flex justify-center items-center'>
-              <AsmaulHusnaCard/>
+            <div className='flex flex-col justify-center items-center mb-[10rem]'>
+              {asmaulHusnaData.map(item => (
+
+                <AsmaulHusnaCard key={item.id} id={item.id_asmaulhusna} arab={item.arab} indo={item.indo} latin={item.latin} />
+
+              ))}
             </div>
+
           </div>
 
         </div>
+      </div>
+
+      <div>
+        <Footer />
       </div>
     </div>
   );
