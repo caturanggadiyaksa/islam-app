5f97e9ced87ab329a8cd3571f3208796a1efc15d
caturanggaadiyaksa <adiyaksacaturangga@gmail.com>
2023-10-30 10:27:31 +0700

    add component footer

--- src/pages/QuranPage.jsx
+++ src/pages/QuranPage.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import NavbarQuran from '../components/navbar/qurannav/NavbarQuran';
 import QuranContainer from '../containers/QuranContainer';
-
+import Footer from '../components/footer/Footer';
 
 const QuranPage = () => {
+
   return(
     <div>
       <NavbarQuran/>
 
       <div className='pt-[6rem]'> 
         <QuranContainer/>
+      </div>
+
+      <div>
+        <Footer/>
       </div>
     </div>
   );
