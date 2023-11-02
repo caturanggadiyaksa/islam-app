d472b35e19f7f99d59e00ee1862057f29540f52c
caturanggaadiyaksa <adiyaksacaturangga@gmail.com>
2023-11-02 16:58:25 +0700

    add navbar ayat

---
+++ src/components/navbar/AyatNav.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeftLong } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+
+
+const AyatNav = () => {
+    return (
+        <>
+            <header
+                className="fixed top-0 z-50 w-full h-[5rem] bg-slate-50 shadow-gray-500 drop-shadow-md"
+            >
+                <div className=" w-full flex justify-center items-center">
+                    <div className="w-full h-[5rem]">
+                        <div className="w-[90%] h-[5rem] mx-auto flex items-center justify-beetwen">
+                            <div className="w-1/2 flex items-center">
+                                <Link to="/quran">
+                                    <FontAwesomeIcon icon={faArrowLeftLong} />
+                                </Link>
+                                {/* <div className="ml-4">
+                                    Ayat
+                                </div> */}
+                            </div>
+
+
+
+
+
+
+                        </div>
+                    </div>
+
+                </div>
+            </header>
+        </>
+    );
+};
+
+export default AyatNav;
