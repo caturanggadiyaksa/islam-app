c27c242d0f0239319f5eda874cf3bbd0a674c8d1
caturanggaadiyaksa <adiyaksacaturangga@gmail.com>
2023-11-02 16:56:08 +0700

    add surah card

--- src/containers/QuranContainer.jsx
+++ src/containers/QuranContainer.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import SurahCard from "../components/cards/SurahCard";
 
+
 const QuranContainer = () => {
-    return(
+
+    const [surahData, setSurahData] = useState([]);
+
+    useEffect(() => {
+
+        const data = localStorage.getItem('surahData');
+        if (data) {
+            setSurahData(JSON.parse(data));
+        }
+
+    }, []);
+
+
+    return (
         <>
             <div className="w-[90%] mx-auto">
                 <div className="w-full">
@@ -14,9 +28,14 @@ const QuranContainer = () => {
                 </div>
 
                 <div className="w-full mt-4">
-                    <div className="flex flex-wrap  justify-center items-center">
-                        <SurahCard/>
-                        
+                    <div className="flex flex-wrap  justify-center items-center mb-[10rem]">
+                        {surahData.map(item => (
+
+                      
+                            <SurahCard key={item.id} tempat={item.revelation_id} total={item.numberOfVerses} arab={item.shortName} number={item.number} name={item.transliteration_id} arti={item.translation_id} />
+                           
+                           
+                        ))}
                     </div>
                 </div>
 
