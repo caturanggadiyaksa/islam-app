2c0344ad722c7df51deaf30395ddbfc98b933d5e
caturanggaadiyaksa <adiyaksacaturangga@gmail.com>
2023-11-02 17:02:09 +0700

    add props

--- src/components/cards/AsmaulHusnaCard.jsx
+++ src/components/cards/AsmaulHusnaCard.jsx
@@ -1,42 +1,50 @@
 import React from "react";
+import './cardstyle.css';
 
-const AsmaulHusnaCard = () => {
-    return(
+
+
+const AsmaulHusnaCard = ({ id, arab, indo, latin }) => {
+
+
+    
+
+    return (
         <>
-            <div className="max-w-[40rem] w-[40rem] bg-white shadow-lg rounded-lg mx-6">
+            <div className="max-w-[40rem] w-[40rem] bg-white shadow-lg rounded-lg my-6">
                 <div className=" w-full flex justify-center items-center p-3">
                     <div className="w-full">
                         <div className="flex items-center pl-2">
+                        
                             <div className="mr-2">
                                 <div className="bg-slate-200 w-10 h-10 rounded-full">
                                     <div className=" w-10 h-10 rounded-full flex justify-center items-center">
                                         <p>
-                                            1
+                                            {id}
                                         </p>
                                     </div>
                                 </div>
                             </div>
                             <div className="w-full flex flex-col justify-center items-center">
                                 <div className="py-4">
-                                    <p className="font-bold text-xl">
-                                        Al-Ma'idah
+                                    <p className=" text-2xl my-font" >
+                                      {arab}
                                     </p>
                                 </div>
                                 <div className="py-2">
                                     <p className="text-lg font-semibold">
-                                        Jamuan(hidangan makanan)
+                                        {latin}
                                     </p>
                                 </div>
 
                                 <div className="py-2">
                                     <p className="text-md font-extralight">
-                                       Madinah
+                                        {indo}
                                     </p>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    
+
                 </div>
             </div>
         </>
