57cc1011b81a997777ee5782ca9f3049a699e910
caturanggaadiyaksa <adiyaksacaturangga@gmail.com>
2023-11-02 16:50:57 +0700

    add ayat card

--- src/pages/AyatPage.jsx
+++ src/pages/AyatPage.jsx
@@ -1,7 +1,50 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import AyatNav from '../components/navbar/AyatNav';
+import ApiAyats from '../service/ApiAyats';
+import AyatCard from '../components/cards/AyatCard';
+
 
 const AyatPage = () => {
-  return <div>Halaman Ayat</div>;
+
+  const { ayatNumber } = useParams();
+
+  const [ayatsData, setAyatsData] = useState([]);
+
+  useEffect(() => {
+
+    const data = localStorage.getItem('ayatsData');
+    if (data) {
+      setAyatsData(JSON.parse(data));
+    }
+
+  }, []);
+
+  return (
+    <>
+      <div>
+        <ApiAyats surahNumber={ayatNumber} />
+        <AyatNav />
+      </div>
+
+      <div className='mt-[8rem] w-full'>
+        <div className='w-[80%] mx-auto'>
+          <div className='flex flex-col justify-center items-center'>
+            {asmaulHusnaData.map(item => (
+
+              <AyatCard key={item.id} number={item.verse_number} ar={arabic_text} text={transliteration_en} id={translation_id} />
+
+            ))}
+          </div>
+        </div>
+
+      </div>
+    </>
+  );
+
 };
 
 export default AyatPage;
+
+
+
